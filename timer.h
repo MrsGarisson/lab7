ae65c270270f7ed401422345b7421140af33cd1c
expsk <pavlovskes@gmail.com>
2013-06-06 16:59:07 +0300

    Module prefix

--- timer.h
+++ timer.h
@@ -1,5 +1,6 @@
 #define MODULE_PREFIX "[TIMER] "
 
-MODULE_AUTHOR("Koloskova Anastasiya and Andrew Fedchuk");
+MODULE_AUTHOR("Aifonova, Dmitrach, Pavlovsky, Pozdnyakov");
+MODULE_AUTHOR("3k 12gr");
 MODULE_DESCRIPTION("Timer Driver");
 MODULE_LICENSE("GPL");
